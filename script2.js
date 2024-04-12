ea6d2820eb0c13fc328b51a33e92af94647e0b79
alejandro0955 <33553049+alejandro0955@users.noreply.github.com>
2024-04-11 23:04:56 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   about.html
    	renamed:    arrow.jpg -> assets/arrow.jpg
    	renamed:    logo.jpg -> assets/logo.jpg
    	renamed:    logo2.jpg -> assets/logo2.jpg
    	renamed:    startButton.jpg -> assets/startButton.jpg
    	renamed:    title.jpg -> assets/title.jpg
    	renamed:    topbutton.jpg -> assets/topbutton.jpg
    	modified:   index.html
    	modified:   script2.js
    	modified:   style2.css

--- script2.js
+++ script2.js
@@ -1,19 +1,19 @@
-var map = L.map('map').setView([25.7994996, -80.1990463], 13);
+var map = L.map('map').setView([25.7994996, -80.1990463], 15.2);
 
-coords = [[25.799528687498434,-80.19929050718713],
-    [25.79945799029953,-80.19952622483581],
-    [25.799855270876456,-80.19894351492732],
-    [25.80008471876335,-80.19926202724784],
-    [25.799874665107403,-80.19833197127193],
-    [25.799164736083736,-80.19892272780746],
-    [25.798884649708405,-80.19918357263415],
-    [25.798823108345555,-80.19898307751029],
-    [25.798914910992224,-80.19860823880046],
-    [25.799814482609975,-80.1981918258509],
-    [25.799500589828693,-80.19993727336734]]
+coords = [[25.797673152092322,-80.20078854661462],
+[25.799930984900485,-80.19905217367373],
+[25.79915251186354,-80.19870740648331],
+[25.800665362118014,-80.20198582903953],
+[25.801455817813252,-80.1985444256297],
+[25.80080151416646,-80.19742236513727],
+[25.799966610413314,-80.19660119237464],
+[25.798823108345555,-80.19898307751029],
+[25.80211223417643,-80.20021811362717],
+[25.802100594369204,-80.20205478247793],
+[25.80222439953184,-80.19819965843949]]
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: 1,
+    maxZoom: 30,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
@@ -27,8 +27,16 @@ var circle = L.circle([25.7994996, -80.1990463], {
 }).addTo(map);
 
 
-for (let i = 0; i < coords.length; i++) {
-    var con = document.getElementById("terminal");
-    var marker = L.marker(coords[i], {}).addTo(map);
-    con.innerHTML += ("Parking found at: " + (coords[i]) + "<br>");
-}
+// for (let i = 0; i < coords.length; i++) {
+//     var con = document.getElementById("terminal");
+//     var marker = L.marker(coords[i], {}).addTo(map);
+//     con.innerHTML += ("> Parking found at: " + (coords[i]) + "<br>");
+// }
+
+for (let i = 1; i < coords.length; i++) {
+    setTimeout(function timer() {
+        var con = document.getElementById("terminal");
+        var marker = L.marker(coords[i], {}).addTo(map);
+        con.innerHTML += ("> Parking found at: " + (coords[i]) + "<br>");
+    }, i * 3000);
+  }
